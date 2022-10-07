f62bc313a1dc532843c881cae59a15e77ce079b3
janssen-v <janssen.vincentius@gmail.com>
2022-10-07 20:52:19 +0000

    kernel module running without creating new thread

--- Assignment1/source/program2/program2.c
+++ Assignment1/source/program2/program2.c
@@ -13,45 +13,69 @@
 MODULE_LICENSE("GPL");
 
 
+struct wait_opts {
+	enum pid_type wo_type;
+	int	wo_flags;					// Wait options
+	struct pid *wo_pid;				// Kernel internal P
+	struct waitid_info *wo_info;	// Signal info
+	int	wo_stat;					// Child process termination status
+	struct rusage *wo_rusage;		// Resource usa
+	wait_queue_entry_t child_wait;	// Task wait queue
+	int	notask_error;
+};
+
+//long do_wait(struct wait_opts *wo);
+
 //implement fork function
-int my_fork(void *argc){
-	
-	
+
+static struct task_struct *task;
+
+int my_fork(void *argc) {
 	//set default sigaction for current process
 	int i;
 	struct k_sigaction *k_action = &current->sighand->action[0];
-	for(i=0;i<_NSIG;i++){
+	for (i = 0; i < _NSIG; i++) {
 		k_action->sa.sa_handler = SIG_DFL;
 		k_action->sa.sa_flags = 0;
 		k_action->sa.sa_restorer = NULL;
 		sigemptyset(&k_action->sa.sa_mask);
 		k_action++;
-	}
-	
+	};
+
 	/* fork a process using kernel_clone or kernel_thread */
 
-	pid_t ret;
-	ret = kernel_clone()
-	
-	/* execute a test program in child process */
-	
-	/* wait until child process terminates */
-	
-	return 0;
-}
+		/* execute a test program in child process */
 
-static int __init program2_init(void){
+		/* wait until child process terminates */
 
-	printk("[program2] : Module_init\n");
+		return 0;
+};
+
+
+
+static int __init program2_init(void)
+{
+	printk("[program2] : Module_init {Vincentius Janssen} {119010518}\n");
+	printk("[program2] : Module_init create kthread start\n");
+	// Create a kthread
+	task = kthread_create(&my_fork, NULL, "Parent Thread");
+	// Wake up kthread if functioning
+	if(!IS_ERR(task)){
+			printk("[program2] : Module_init kthread start\n");
+			wake_up_process(task);
+	}
 	
-	/* write your code here */
-	
+
+
 	/* create a kernel thread to run my_fork */
-	
+
+
+
 	return 0;
 }
 
-static void __exit program2_exit(void){
+static void __exit program2_exit(void)
+{
 	printk("[program2] : Module_exit\n");
 }
 
